c698d178a3605f7db9dd537570f8a1797f7e86e3
Alessandro Orrù <alessandro.orru@moneyfarm.com>
2015-10-19 15:37:07 +0200

    Converted Log to an Enum

--- Log.swift
+++ Log.swift
@@ -1,59 +1,104 @@
-struct Log {
-    enum Level : Int {
-        case Trace, Debug, Info, Warning, Error, None
+public enum Log : Int {
+    case Trace, Debug, Info, Warning, Error, None
+
+    public typealias PrintFunction = (items: Any..., separator: String, terminator: String) -> Void
+    public typealias SimpleFormatter = (object: Any) -> String
+    public typealias ExtendedFormatter = (object: Any, file: String, line: Int, function: String) -> String
+    public typealias ColorForLevel = (level: Log) -> UIColor
+    
+    public static var Level : Log = Log.Error
+    public static var printFunction : PrintFunction = Swift.print
+    public static var simpleFormatter : SimpleFormatter = Log.format
+    public static var extendedFormatter : ExtendedFormatter = Log.format
+    public static var colorForLevel : ColorForLevel = Log.colorForLevel
+    
+    public static var dateFormatter : NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .NoStyle
+        dateFormatter.timeStyle = .MediumStyle
+        dateFormatter.locale = NSLocale.systemLocale()
+        return dateFormatter
+    }()
+    
+    public func print(object:Any, extended: Bool = true, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
+        guard rawValue >= Log.Level.rawValue else {
+            return
+        }
+        
+        let color = colorString()
+        var formattedString : String
+        switch extended {
+        case false:
+            formattedString = Log.simpleFormatter(object: object)
+        case true:
+            formattedString = Log.extendedFormatter(object: object, file: file, line: line, function: function)
+        }
+        
+        if Log.colorsEnabled {
+            formattedString = "\(Log.ESCAPE)\(color)\(formattedString)\(Log.RESET)"
+        }
+        
+        Log.printFunction(items: formattedString, separator: ",", terminator: "\n")
     }
     
-    private static let ESCAPE = "\u{001b}["
     
-    private static let RESET_FG = ESCAPE + "fg;" // Clear any foreground color
-    private static let RESET_BG = ESCAPE + "bg;" // Clear any background color
-    private static let RESET = ESCAPE + ";"   // Clear any foreground or background color
+    // MARK: - Private
+    private func colorString() -> String {
+        let color = Log.colorForLevel(self)
+        
+        var r : CGFloat = 0
+        var g : CGFloat = 0
+        var b : CGFloat = 0
+        var a : CGFloat = 0
+        color.getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        return String(format: "fg%.0f,%.0f,%.0f;", arguments: [round(r*255), round(g*255), round(b*255)])
+    }
+    
+    private static func colorForLevel(level: Log) -> UIColor {
+        switch level {
+        case .Trace:
+            return UIColor.whiteColor()
+        case .Debug:
+            return UIColor.darkGrayColor()
+        case .Info:
+            return UIColor.yellowColor()
+        case .Warning:
+            return UIColor.orangeColor()
+        case .Error:
+            return UIColor.redColor()
+        default:
+            return UIColor.whiteColor()
+        }
+    }
+    
+    private static func format(object: Any) -> String {
+        return "\(Log.dateFormatter.stringFromDate(NSDate())): \(object)"
+    }
+
+    private static func format(object: Any, file: String, line: Int, function: String) -> String {
+        let file = file.componentsSeparatedByString("/").last!
+        var logString = "\n\n[\(file):\(line)] \(function) | \n"
+        logString += "\(Log.dateFormatter.stringFromDate(NSDate())): \(object)"
+        logString += "\n"
+        
+        return logString
+    }
     
     private static var colorsEnabled: Bool = {
-        let xcodeColors = getenv("XcodeColors")
-        let xcodeColorsAvailable = xcodeColors != nil && strcmp(xcodeColors, "YES") == 0
+        let xcodeColors = NSProcessInfo().environment["XcodeColors"]
+        let xcodeColorsAvailable = xcodeColors != nil && xcodeColors == "YES"
         return xcodeColorsAvailable
     }()
     
-    static func error<T>(object:T, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-        let color = "fg255,0,0;";
-        print(object, level: .Error, colorString: color, file: file, line: line, function: function)
-    }
+    private static let ESCAPE = "\u{001b}["
+    private static let RESET_FG = "\(ESCAPE) fg;" // Clear any foreground color
+    private static let RESET_BG = "\(ESCAPE) bg;" // Clear any background color
+    private static let RESET = "\(ESCAPE);"   // Clear any foreground or background color
+}
 
-    static func warning<T>(object:T, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-        let color = "fg255,127,0;";
-        print(object, level: .Warning, colorString: color, file: file, line: line, function: function)
-    }
-    
-    static func info<T>(object:T, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-        let color = "fg255,255,0;";
-        print(object, level: .Info, colorString: color, file: file, line: line, function: function)
-    }
 
-    static func debug<T>(object:T, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-        let color = "fg70,70,70;";
-        print(object, level: .Debug, colorString: color, file: file, line: line, function: function)
-    }
-
-    static func trace<T>(object:T, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
-
-        let color = "fg255,255,255;";
-        print(object, level: .Trace, colorString: color, file: file, line: line, function: function)
-    }
-
-    private static func print<T>(object:T, level: Log.Level, colorString: String = "", file: String, line: Int, function: String) {
-        
-        if LogLevel.rawValue > level.rawValue { return }
-        
-        let file = file.lastPathComponent
-        var logString = "[\(file):\(line)] \(function) | \n"
-        logString += "\(object)"
-        logString += "\n\n"
-        
-        if self.colorsEnabled {
-            println("\(ESCAPE)\(colorString)\(logString)\(RESET)")
-        } else {
-            println("\(logString)")
-        }
-    }
+infix operator => {}
+public func =>(lhs: Any, rhs: Log) {
+    rhs.print(lhs, extended: false)
 }
--- README.md
+++ README.md
@@ -1,31 +1,109 @@
-# SimpleSwiftLogger
-Simple Swift logger with colors support.
-It started as a simple single-project only file, but I would like to extend it.
+# The damn simple Swift Logger
+Lightweight Swift logger with color support.
+
 
 ## Install
-Drop in your project and configure your desired log level like this:
+Drop `Log.swift` in your project and configure your desired log level like this:
 
 ```
 #if DEBUG
-let LogLevel : Log.Level = .Trace
+Log.Level = .Trace
 #else
-let LogLevel : Log.Level = .Error
+Log.Level = .Error
 #endif
 ```
 
 ## Usage
-Call the method referring to the desided log level:
+The available levels are `Trace`, `Debug`, `Info`, `Warning`, `Error`. There is also a `None` level that can be used to disable logging.
 
-`Log.error("Fatal error!")`
+You can log using two different fashions:
 
-At the moment the available levels are `error`, `warning`, `info`, `debug`, `trace`.
+1. **Simple**, without annotations about file, line and function name.
+2. **Extended**, with annotations about file, line and function name.
 
-## Customization
-There is no direct way to customize it at the moment, except editing the main file. The core function is the `print` method that formats the log. Colors are defined inside the level methods.
+### Simple logging
+You can log using either the `=>` operator or the `print` function (passing `false` to the `extended` parameter)
+
+Example:
+
+```
+"Fatal error!" => Log.Error
+// or
+Log.Error.print("Fatal error!", extended: false)
+
+// will both print 
+3:12:14 PM: Fatal error!
+```
+
+
+
+
+
+### Extended logging
+If you need more details, you can use the `print` function, without passing any parameter.
+
+Example:
+
+```
+Log.Error.print("Fatal error!")
+
+// Will print:
+[AppDelegate.swift:28] application(_:didFinishLaunchingWithOptions:) | 
+3:23:26 PM: Fatal error!
+```
+
+
 
 ## Colors support
 
 Just install [XcodeColors](https://github.com/robbiehanson/XcodeColors), and it will just work!
+
+
+
+## Customization
+
+### Print format
+You can format both the simple and the extended log as you want, by replacing the default `Log.simpleFormatter` and `Log.extendedFormatter` functions.
+
+Example:
+
+```
+Log.simpleFormatter = { object in
+	return "Object: \(object)"
+}
+```
+
+### Print function
+By default, Log will print using the standard `Swift.print` function. If you prefer to print using your custom implementation (i.e. if you want to log to a file), just hook a function to `Log.printFunction` conforming to this signature `(items: Any..., separator: String, terminator: String) -> Void`.
+
+
+### Colors
+You can define your own colors providing a new function to the `colorForLevel` property, with signature `(level: Log) -> UIColor`. 
+
+
+### Date formatter
+You can define your own date formatter replacing the default `Log.dateFormatter`.
+
+
+## Known issues and limitations
+
+There are both **simple** and **extended** logging due to an operator limit: to add the file, line and function information I need to pass them as default parameters of a function, but this isn't possible on operator functions.
+
+To better explain, the `print` function is defined like this:
+
+`public func print(object:Any, extended: Bool = true, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__)`
+ 
+Unfortunately, I can't do a similar thing with the operator function:
+
+`public func =>(lhs: Any, rhs: Log, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__)`
+
+Will fail with:
+> Operators must have one or two arguments
+
+So I can't find a way to log the extended info while using the `=>` operator.
+
+If you find a way to solve it, please open a new issue or a pull request.
+
 
 ## Contributing
 
